3a6de85c59a330250803ff2ce32da0ca4a37c85a
David Keijser <keijser@gmail.com>
2014-09-20 17:54:41 +0200

    include sub-dependecies

--- test-run.js
+++ test-run.js
@@ -1,6 +1,32 @@
 var dd = require('./lib/dependency-details'),
     npmconf = require('npmconf');
 
+function ws(n) {
+    var out = '';
+    while (n-- > 0) {
+        out += ' ';
+    }
+    return out;
+}
+
+function pad(s, n) {
+    s = '' + s;
+    return s + ws(n - s.length);
+}
+
+function outputTable(details, level) {
+    level = level || 0;
+    for (var key in details) {
+        data = details[key];
+        console.log(pad(ws(level) + key, 32),
+                    '\t', pad(data.current, 12),
+                    '\t', pad(data.installed, 12),
+                    '\t', pad(data.latest, 12),
+                    data.dev ? '[DEV]' : '');
+        outputTable(data.dependencies, level + 1)
+    }
+}
+
 npmconf.load({}, function (err, config) {
     dd(config, process.argv[2], function (err, details) {
         var key,
@@ -10,9 +36,6 @@ npmconf.load({}, function (err, config) {
             return console.error(err);
         }
 
-        for (key in details) {
-            data = details[key];
-            console.log(key, data.current, data.installed, data.latest, data.dev ? '[DEV]' : '');
-        }
+        outputTable(details);
     });
 });
