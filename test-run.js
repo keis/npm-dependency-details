6ff7be0e16eb7426d5e6d3a2167e4a022763f081
David Keijser <keijser@gmail.com>
2015-08-28 19:02:30 +0200

    javascript standard style

--- test-run.js
+++ test-run.js
@@ -1,43 +1,46 @@
-var dd = require('./lib/dependency-details'),
-    npmconf = require('npmconf');
+var dd = require('./lib/dependency-details')
+var npmconf = require('npmconf')
 
-function ws(n) {
-    var out = '';
-    while (n-- > 0) {
-        out += ' ';
-    }
-    return out;
+function ws (n) {
+  var out = ''
+  while (n-- > 0) {
+    out += ' '
+  }
+  return out
 }
 
-function pad(s, n) {
-    s = '' + s;
-    return s + ws(n - s.length);
+function pad (s, n) {
+  s = '' + s
+  return s + ws(n - s.length)
 }
 
-function outputTable(details, level) {
-    level = level || 0;
-    for (var key in details) {
-        data = details[key];
-        console.log(pad(ws(level) + key, 32),
-                    '\t', pad(data.current, 12),
-                    '\t', pad(data.installed, 12),
-                    '\t', pad(data.latest, 12),
-                    data.dev ? '[DEV]' : '');
-        outputTable(data.dependencies, level + 1)
-    }
+function outputTable (details, level) {
+  var data
+  var key
+  level = level || 0
+  for (key in details) {
+    data = details[key]
+    console.log(pad(ws(level) + key, 32),
+          '\t', pad(data.current, 12),
+          '\t', pad(data.installed, 12),
+          '\t', pad(data.latest, 12),
+          data.dev ? '[DEV]' : '')
+    outputTable(data.dependencies, level + 1)
+  }
 }
 
 npmconf.load({}, function (err, config) {
-    var options = {config: config};
+  var options = {config: config}
 
-    dd(options, process.argv[2], function (err, details) {
-        var key,
-            data;
+  if (err) {
+    return console.error(err)
+  }
 
-        if (err) {
-            return console.error(err);
-        }
+  dd(options, process.argv[2], function (err, details) {
+    if (err) {
+      return console.error(err)
+    }
 
-        outputTable(details);
-    });
-});
+    outputTable(details)
+  })
+})
